6e11723003522f848d642185813d525412e3df6d
Huzifa Kabakibo <huzifa.kab@gmail.com>
2022-08-12 19:07:17 +0300

    almost finished

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ function showMenu() {
     menu.style.display = "none";
   }
 }
+
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days p").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours p").innerHTML =
+    hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes p").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds p").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
